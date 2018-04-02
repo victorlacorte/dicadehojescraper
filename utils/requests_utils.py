b10c9a094150676b1b84d4c003c0274b23ab182e
vlacorte <victor.lacorte@editoracubo.com.br>
2018-04-02 17:15:54 -0300

    Included bisect insert/lookup and better MIME support

--- utils/requests_utils.py
+++ utils/requests_utils.py
@@ -1,17 +1,20 @@
+import bisect
 from bs4 import BeautifulSoup
-import os
+import re
 import requests
+
+from utils.sort import index
 
 
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
-def is_downloadable(url, session=None, mimes=['text/html']):
+def is_downloadable(url, mimes, session=None):
     """
         Return True if the URL Content-Type entity header contains a MIME type
             that suggest a downloadable URL
         :param url: URL to analyze
-        :param session: http://docs.python-requests.org/en/master/user/advanced/#session-objects
-        :param mimes: values that suggest a _non_ downloadable URL
+        :param mimes: values that suggest a downloadable URL
+        :param (optional) session: http://docs.python-requests.org/en/master/user/advanced/#session-objects
     """
     try:
         if session:
@@ -22,40 +25,41 @@ def is_downloadable(url, session=None, mimes=['text/html']):
         # The best approach would be to log this failed HEAD request attempt
         return False
     content_type = r.headers.get('content-type')
-    if content_type:
-        # If we find any match on the MIMEs list the URL is _not_ downloadable,
-        # so we negate the result to obtain the desired return value
-        return not any(_ in content_type.lower() for _ in mimes)
+    if content_type: 
+        for m in mimes:
+            if re.findall(m, content_type):
+                return True
     return False
-def downloadable_urls(urls, session, domains, visited=[]):
+def downloadable_urls(urls, session, domains, mimes, visited=[]):
     while urls:
         url = urls.pop()
-        print(url)
-        # Probably don't need to verify if the URL is within the domains
-        #   since we do that when adding the hrefs
-        #if any(_ in url for _  in domains) and url not in visited:
-        if url not in visited:
-            visited.append(url)
-            if is_downloadable(url, session):
-                yield url
+        if is_downloadable(url, mimes, session):
+            yield url
+        else:
+            # Save bandwidth (and time) by performing a GET on
+            #   non-downloadable URLs
+            try:
+                r = session.get(url)
+            except requests.exceptions.RequestException:
+                # Could also log the exception for future inspection
+                pass
             else:
-                # Save bandwidth (and time) by performing a GET on
-                #   non-downloadable URLs
-                try:
-                    r = session.get(url)
-                except requests.exceptions.RequestException:
-                    # Could also log the exception for future inspection
-                    pass
-                else:
-                    soup = BeautifulSoup(r.text, 'lxml')
-                    for link in soup.find_all('a'):
-                        href = link.get('href')
-                        # Another optimization: only add hrefs within the
-                        #   domains 
-                        if href and any(_ in href for _ in domains):
-                            urls.add(href)
-                    yield from downloadable_urls(urls, session, domains,
-                            visited)
+                soup = BeautifulSoup(r.text, 'lxml')
+                for link in soup.find_all('a'):
+                    href = link.get('href')
+                    # Another optimization: only add hrefs within the
+                    #   domains 
+                    if href:
+                        try:
+                            _ = index(visited, href)
+                        except ValueError:
+                            bisect.insort(visited, href)
+                            for domain in domains:
+                                if re.findall(domain, href):
+                                    print(href)
+                                    urls.add(href)
+                yield from downloadable_urls(urls, session, domains,
+                            mimes, visited)
 def download(url, saveas, stream=False, chunk_size=None):
     r = requests.get(url, stream=stream)
     with open(saveas, 'wb') as f:
