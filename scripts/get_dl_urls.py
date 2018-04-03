6351ba302e3b73955c8f5a6b0c83a1d5d32baaa7
vlacorte <victor.lacorte@editoracubo.com.br>
2018-04-03 17:21:10 -0300

    Included "User-Agent" and sleep() but the code still is not deterministic

--- scripts/get_dl_urls.py
+++ scripts/get_dl_urls.py
@@ -4,14 +4,21 @@ import sys
 from utils.requests_utils import downloadable_urls
 from sensitive_info import *
 
+
 reports = f'{base_url}/carteira-dica-de-hoje-relatorios'
 login = f'{base_url}/wp-login.php'
 # https://www.iana.org/assignments/media-types/media-types.xhtml
 mimes_csv = 'data/mime_types/mimes.csv'
+# http://www.useragentstring.com/index.php?id=19830
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)' \
+        ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1' \
+        ' Safari/537.36'}
+timeout = (5, 10)
 
 if __name__ == '__main__':
     fname = sys.argv[1]
     with requests.Session() as s:
+        s.headers.update(headers)
         s.post(login, data=login_payload)
         mimes = set()
         with open(mimes_csv, 'r') as m:
@@ -21,5 +28,6 @@ if __name__ == '__main__':
                 if r:
                     mimes.add(r)
         with open(fname, 'w') as f:
-            for ret in downloadable_urls({base_url}, s, domains, mimes):
+            for ret in downloadable_urls({base_url}, s, domains, mimes,
+                    timeout_min=timeout[0], timeout_max=timeout[1]):
                 print(ret, file=f)
--- utils/requests_utils.py
+++ utils/requests_utils.py
@@ -1,48 +1,58 @@
 import bisect
 from bs4 import BeautifulSoup
+from random import randint
 import re
 import requests
+from time import sleep
 
 from utils.sort import index
 
 
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
-def is_downloadable(url, mimes, session=None):
+def is_downloadable(url, mimes, session=None, timeout_min=5, timeout_max=10):
     """
         Return True if the URL Content-Type entity header contains a MIME type
             that suggest a downloadable URL
-        :param url: URL to analyze
         :param mimes: values that suggest a downloadable URL
         :param (optional) session: http://docs.python-requests.org/en/master/user/advanced/#session-objects
+        :param (optional) timeout: timeout in seconds between each HEAD request
     """
+    sleep(randint(timeout_min, timeout_max))
     try:
         if session:
             r = session.head(url)
         else:
             r = requests.head(url)
-    except requests.exceptions.RequestException:
-        # The best approach would be to log this failed HEAD request attempt
-        return False
+    except requests.exceptions.RequestException as e:
+        # The best approach would be to log this failed HEAD request
+        #   attempt
+        print(f'HEAD request on {url} raised {e}')
+        return False 
     content_type = r.headers.get('content-type')
     if content_type: 
+        # This could be optimized (although set lookups are very fast). See
+        # https://stackoverflow.com/questions/7380629/perform-a-binary-search-for-a-string-prefix-in-python
         for m in mimes:
             if re.findall(m, content_type):
-                return True
+               return True 
     return False
-def downloadable_urls(urls, session, domains, mimes, visited=[]):
+def downloadable_urls(urls, session, domains, mimes, visited=[], timeout_min=5,
+        timeout_max=10):
     while urls:
         url = urls.pop()
-        if is_downloadable(url, mimes, session):
+        if is_downloadable(url, mimes, session, timeout_min, timeout_max):
             yield url
         else:
             # Save bandwidth (and time) by performing a GET on
             #   non-downloadable URLs
+            sleep(randint(timeout_min, timeout_max))
             try:
                 r = session.get(url)
-            except requests.exceptions.RequestException:
+            except requests.exceptions.RequestException as e:
+                print(f'HEAD request on {url} raised {e}')
                 # Could also log the exception for future inspection
-                pass
+                continue
             else:
                 soup = BeautifulSoup(r.text, 'lxml')
                 for link in soup.find_all('a'):
@@ -52,14 +62,13 @@ def downloadable_urls(urls, session, domains, mimes, visited=[]):
                     if href:
                         try:
                             _ = index(visited, href)
-                        except ValueError:
+                        except ValueError:  # href not in visited
                             bisect.insort(visited, href)
                             for domain in domains:
                                 if re.findall(domain, href):
-                                    print(href)
                                     urls.add(href)
                 yield from downloadable_urls(urls, session, domains,
-                            mimes, visited)
+                            mimes, visited, timeout_min, timeout_max)
 def download(url, saveas, stream=False, chunk_size=None):
     r = requests.get(url, stream=stream)
     with open(saveas, 'wb') as f:
